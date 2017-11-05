e57b52bd091e097c44020348a2c648d1614bb34f
Your Name <emonsafayet@gmail.com>
2017-11-06 01:53:40 +0600

    connected with db

---
+++ Lbl-ServerApp/Models/LmsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lbl_ServerApp.Models
+{
+    public class LmsDbContext : DbContext
+    {
+        public LmsDbContext() : base("DefaultConnection")
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
